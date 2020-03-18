a47c5f6a4f53af101c27654227c4cb77cb5c58e0
Alex Antoniades <antoniades.alex@gmail.com>
2020-03-18 00:44:46 +0000

    Added README.md

---
+++ README.md
@@ -0,0 +1,24 @@
+# bear-cli
+
+## Install
+```bash
+git clone https://github.com/alexantoniades/bear-cli.git
+cd bear-cli
+pip3 install -r requirements.txt
+alias bear="$(which python3) $(pwd)/bear.py"
+
+bear --help
+```
+## Usage
+### Create profile
+```bash
+bear add-profile -n <profile name> -k <api key>
+```
+### Remove profile
+```bash
+bear remove-profile -n <profile name>
+```
+### Call API
+```bash
+bear api -p <profile name> -a <action>
+```
--- bear.py
+++ bear.py
@@ -1,101 +1,102 @@
-import click
-import requests
-import os
-import json
-from src.access import User, HiddenPassword
+import click, os, requests
+from tinydb import TinyDB, Query
 
+class ApiDB:
+    def __init__(self, path='./profiles/profiles.json'):
+        self.path = path
+        self.document = TinyDB(path)
+
+    def addAPIKey(self, name, key):
+        return(self.document.upsert({'name': name, 'key': key}, Query().name == name))
+
+    def findAPIKey(self, name):
+        profile = self.document.search(Query().name == name)
+        if len(profile) == 0:
+            return('')
+        else:
+            return(profile[0]['key'])
+
+    def removeAPIKey(self, name):
+        return(self.document.remove(Query().name == name))
+
+    @property
+    def len(self):
+        return(len(self.document))
+
+def callAPI(action, params=dict(), api_key='', url='https://qgg.hud.ac.uk/controller/api.php'):
+    """GET requests API: a: action, params: parameters."""
+    headers={ 'API_KEY': api_key }
+    parameters={ 'a': action, **params }
+    return(requests.get(
+        url=url,
+        headers=headers,
+        params=parameters
+    ).json())
+
+# Declare database object
+db = ApiDB()
 
 @click.group(invoke_without_command=False)
 @click.pass_context
 def bear(context):
-    if context.invoked_subcommand is None:
-        print('I was invoked without subcommand')
-    else:
-        print('I am about to invoke %s' % context.invoked_subcommand)
+    pass
 
-
-# bear <login>
+# bear <add-profile>
 @bear.command()
 @click.option(
-    '--email',
-    prompt=True,
-    default=lambda: os.environ.get('USER', '')
+    '--name', '-n',
+    prompt=True
 )
 @click.option(
-    '--password',
-    prompt=True,
-    default=lambda: HiddenPassword(os.environ.get('PASSWORD', '')),
-    hide_input=True
+    '--key', '-k',
+    prompt=True
 )
+def add_profile(name, key):
+    """Adds profile from name and key"""
+    db.addAPIKey(name, key)
+    click.echo(f"'{name}' profile added")
+
+
+# bear <remove-profile>
+@bear.command()
 @click.option(
-    '--tfa',
-    prompt=True,
-    default=lambda: os.environ.get('TWOFACTOR', '')
+    '--name', '-n',
+    prompt=True
 )
 @click.option('--confirm', '-y', is_flag=True, default=False)
-def login(email, password, tfa, confirm):
-    """ 
-    Log in as user
-    """
-    if confirm is not True:
-        check = click.prompt('Are you sure you want to login?(y/n)').lower()
-        if check in ('y', 'yes'):
-            confirm = True
-        else:
-            confirm = False
-
+def remove_profile(name, confirm):
+    """Removes profile by name"""
     if confirm:
-        user = User(email, password, tfa)
-        response = user.login()
-        print(response.text)
+        db.removeAPIKey(name)
+        click.echo(f"'{name}' profile removed")
     else:
-        print('exited')
+        click.echo(f"'{name}' profile not removed")
 
-
-# bear <logout>
+# bear <api>
 @bear.command()
-@click.option('--yes', '-y', is_flag=True, default=False)
-def logout(yes):
-    if yes is not True:
-        confirm = click.prompt('Are you sure?(y/n)').lower()
-        if confirm in ('y', 'yes'):
-            yes = True
+@click.option(
+    '--profile', '-p',
+    prompt=True
+)
+@click.option(
+    '--action', '-a',
+    prompt=True
+)
+@click.option(
+    '--user', '-u',
+    type=None
+)
+def api(profile, action, user):
+    """GET request <action> to api adding key stored in profiles"""
+    key = db.findAPIKey(profile)
+    if key == '':
+        click.echo('Profile does not exists')
+    else:
+        if user is not None:
+            user = {}
         else:
-            yes = False
-
-    
-    if yes:
-        print('logout')
-    else:
-        print('exited')
-    
-
-
-# bear user
-@bear.command()
-@click.option('--all', '-a', type=str)
-def user(all):
-    print('user')
-
-
-# bear view
-@bear.command()
-@click.option('--all', '-a', type=str)
-def view(all):
-    print('view')
-
-
-# bear <system>
-@bear.command()
-@click.option('--install', '-i', is_flag=True, default=False)
-@click.option('--remove', '-r', is_flag=True, default=False)
-def system(install, remove):
-    if install:
-        print('install')
-    elif remove:
-        print('remove')
-    print('system')
-
+            user = { 'u': user }
+        click.echo(str(callAPI(action, user, api_key=key)))
 
 if __name__ == '__main__':
     bear()
